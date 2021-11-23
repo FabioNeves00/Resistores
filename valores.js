200963f7d57194a3aa5d01b14de9dda186368240
FabioNeves00 <fabinhoneves09@gmail.com>
2021-11-22 21:39:32 -0300

    bug correction

--- valores.js
+++ valores.js
@@ -1,6 +1,7 @@
 function show() {
   const resistor = new Resistor()
   let unit = document.getElementById('units')
+  if(document.getElementById('resInput').value == '') {alert('Insira um valor na caixa')}
   let resis = Number(document.getElementById('resInput').value) * (10 ** unit.options[unit.selectedIndex].value)
   resistor.setResistencia(resis)
   document.getElementById('color_list').innerHTML = resistor.getOhms('valor').resistor
