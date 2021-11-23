3e7a83139c953cdc606bba14f1507b6ca73137c3
FabioNeves00 <fabinhoneves09@gmail.com>
2021-11-23 14:54:35 -0300

    New Rules for Resistor

--- Resistor.js
+++ Resistor.js
@@ -4,7 +4,7 @@ class Resistor {
     #min
     #calc
     #resistencia
-    #lista_cores = ["Preto","Marrom","Vermelho","Laranja","Amarelo","Verde","Azul","Violeta","Cinza","Branco","Dourado", "Prateado"]
+    #lista_cores = ["Preto", "Marrom", "Vermelho", "Laranja", "Amarelo", "Verde", "Azul", "Violeta", "Cinza", "Branco", "Dourado", "Prateado"]
     constructor() {
         //clicker do bolsonaro
     }
@@ -64,9 +64,6 @@ class Resistor {
 
     sumCores() {
         switch (this.getCores().length) {
-            case 3:
-                this.#calc = Number(this.getCores()[0] + this.getCores()[1]) * 10 ** Number(this.getCores()[2])
-                return [this.#calc, this.#calc * 20 / 100, Number(this.getCores()[2])]
             case 4:
                 this.#calc = Number(this.getCores()[0] + this.getCores()[1]) * 10 ** Number(this.getCores()[2])
                 return [this.#calc, this.#calc * Number(this.getCores()[3]) / 100, Number(this.getCores()[2])]
@@ -81,8 +78,8 @@ class Resistor {
             case 'cor':
                 this.#max = this.#format(this.sumCores()[0] + this.sumCores()[1], this.sumCores()[2])
                 this.#min = this.#format(this.sumCores()[0] - this.sumCores()[1], this.sumCores()[2])
-                return [this.#min.replace('.', ','), this.#max.replace('.', ',')]
-        
+                return [this.#min, this.#max]
+
             case 'valor':
                 return this.#calcCores(this.#resistencia)
         }
@@ -90,10 +87,6 @@ class Resistor {
 
     #format(total, zeros) {
         switch (zeros) {
-            case 0:
-            case 1:
-            case 2:
-                return `${total} Ω`
             case 3:
             case 4:
             case 5:
@@ -104,68 +97,69 @@ class Resistor {
                 return `${total / 1000000}M Ω`
             case 9:
                 return `${total / 1000000000}G Ω`
-
+            default:
+                return `${total} Ω`
         }
     }
     #calcCores(value) {
-      const res = String(value).split('');
-      for (let i in res) {
-        if (/[^0-9]/.test(res[i])) {
-            res.pop(i);
-            i--;
+        const res = String(value).split('');
+        for (let i in res) {
+            if (/[^0-9]/.test(res[i])) {
+                res.pop(i);
+                i--;
+            }
         }
-      }
-      const resistor = [];
+        const resistor = [];
 
-      for (let i = 0; i < 2; i++)
-          resistor.push(this.#lista_cores[Number(res[i])]);
+        for (let i = 0; i < 2; i++)
+            resistor.push(this.#lista_cores[Number(res[i])]);
 
-      if (res.length < 3)  {
-          resistor.push(this.#lista_cores[0]);
-          if (res.length < 2) { 
-              resistor[1] = this.#lista_cores[0];
-              resistor[2] = this.#lista_cores[10];
-          }
-      } else if (res.length <= 12) {
-          if (res[2] != 0 || (res[2] == 0 && res.length == 12)) {
-            resistor.push(this.#lista_cores[Number(res[2])]);
-            resistor.push(this.#lista_cores[res.length - 3]);
-          } else {
-            resistor.push(this.#lista_cores[res.length - 2]);
-          }
-      } else {
-        while (resistor.length) resistor.pop();
+        if (res.length < 3) {
+            resistor.push(this.#lista_cores[0]);
+            if (res.length < 2) {
+                resistor[1] = this.#lista_cores[0];
+                resistor[2] = this.#lista_cores[10];
+            }
+        } else if (res.length <= 12) {
+            if (res[2] != 0 || (res[2] == 0 && res.length == 12)) {
+                resistor.push(this.#lista_cores[Number(res[2])]);
+                resistor.push(this.#lista_cores[res.length - 3]);
+            } else {
+                resistor.push(this.#lista_cores[res.length - 2]);
+            }
+        } else {
+            while (resistor.length) resistor.pop();
 
-        resistor.push('Inválido');
-      }
+            resistor.push('Inválido');
+        }
 
-      const input = Number(res.join(''))
+        const input = Number(res.join(''))
 
-      return {
-          input,
-          resistor,
-          resto: input - this.#calcResistor(resistor)
-      };
+        return {
+            input,
+            resistor,
+            resto: input - this.#calcResistor(resistor)
+        };
     }
 
     #calcResistor(array) {
-      const res = [];
-  
-      if (array.length == 3) {
-        if (array.indexOf('Dourado')) {
-          for (let i = 0;i < 2;i++) res.push(this.#lista_cores.indexOf(array[i]));
-  
-          for (let i = 0;i < this.#lista_cores.indexOf(array[2]);i++) res.push(0);
+        const res = [];
+
+        if (array.length == 3) {
+            if (array.indexOf('Dourado')) {
+                for (let i = 0; i < 2; i++) res.push(this.#lista_cores.indexOf(array[i]));
+
+                for (let i = 0; i < this.#lista_cores.indexOf(array[2]); i++) res.push(0);
+            } else {
+                return this.#lista_cores.indexOf(array[0]);
+            }
         } else {
-          return this.#lista_cores.indexOf(array[0]);
+            for (let i = 0; i < 3; i++) res.push(this.#lista_cores.indexOf(array[i]));
+
+            for (let i = 0; i < this.#lista_cores.indexOf(array[3]); i++) res.push(0);
         }
-      } else {
-        for (let i = 0;i < 3;i++) res.push(this.#lista_cores.indexOf(array[i]));
-  
-        for (let i = 0;i < this.#lista_cores.indexOf(array[3]);i++) res.push(0);
-      }
-  
-      return Number(res.join(''));
+
+        return Number(res.join(''));
     }
 
 }
